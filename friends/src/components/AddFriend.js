f1df48213fa6dc30e73dd5163a0cf46deea2827e
shanon <13fritsh@gmail.com>
2020-07-21 17:46:55 -0500

    added my post friend function

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -2,20 +2,43 @@ import React from 'react';
 import axios from 'axios';
 
 class AddFriend extends React.Component {
+    initialValues = {
+        name: '',
+        age: '',
+        email: '',
+    };
+
     state = {
-        friend: {
-            name: '',
-            age: '',
-            email: ''
-        }
+        friend: [],
+        formValues: this.initialValues
     };
 
     handleChange = event =>{
         this.setState({
-            friend: {
-                ...this.state.friend,
+            formValues: {
+                ...this.state.formValues,
                 [event.target.name]: event.target.value
             }
         });
+    };
+
+    postFriends = (newFriend) =>{
+        axios.post('http://localhost:5000/api/friends', newFriend)
+            .then(response =>{
+                this.setState({
+                    friend: [
+                        ...this.state.friend,
+                        response.data
+                    ]
+                });
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+            .finally(() =>{
+                this.setState({
+                    formValues: this.initialValues
+                })
+            });
     };
 };
