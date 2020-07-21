e8f7e5c37585f3f53564e0c0c7d7305eb304e841
shanon <13fritsh@gmail.com>
2020-07-21 17:17:45 -0500

    created the start of the add friend form and set my state

---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+class AddFriend extends React.Component {
+    state = {
+        friend: {
+            name: '',
+            age: '',
+            email: ''
+        }
+    };
+};
